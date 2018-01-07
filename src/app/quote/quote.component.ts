d1d1f91fff91c7e0fe1fc232b31473d29957f74b
brilliant kaimba <briegrant416@gmail.com>
2018-01-08 00:21:26 +0300

    Add all

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
     quotes = [
-        new Quote(1,'Watch Finding Nemo','Find an online version and watch merlin find his son',new Date(2018,3,14)),
-        new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
-        new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
-        new Quote(4, 'Get Dog Food','Pupper likes expensive sancks'),
-        new Quote(5, 'Solve math homework','Damn Math'),
-        new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord'),
+        new Quote(1,'Napoleon Hill','Whatever the mind of man can conceive and believe, it can achieve.',new Date(2018,3,14)),
+        new Quote(2,'Jim Rohn','It takes half your life before you discover life is a do-it-yourself project.'),
+        new Quote(3, 'Jeff Bezoz','Diana has her birthday coming up soon'),
+        new Quote(4, 'Robert Kiyosaki','Pupper likes expensive sancks'),
+        new Quote(5, 'Earl Nitingale','Damn Math'),
+        
 
     ]
 
